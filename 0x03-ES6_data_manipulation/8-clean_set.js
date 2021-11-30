e208c9c74c0b26fe331528afbb34b2f9f5571971
coosoti <charlesosoti@gmail.com>
2021-11-30 13:00:53 +0300

    [Set Arrays DS] added  a function named cleanSet that returns a string of all the set values that start with a specific string (startString).
    It accepts two arguments: a set (Set) and a startString (String).
    
    When a value starts with startString you only append the rest of the string. The string contains all the values of the set separated by -.

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,10 @@
 function cleanSet(set, startString) {
   const newList = [];
+  if (startString === '') {
+    return '';
+  }
   for (const elem of set) {
-    if (elem.startsWith(startString) && startString !== '') {
+    if (elem.startsWith(startString) && typeof startString === 'string') {
       newList.push(elem.slice(startString.length));
     }
   }
