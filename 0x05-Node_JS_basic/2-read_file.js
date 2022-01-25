95a22aa13c2555d5860a74305dc91c8c2bd3a516
coosoti <charlesosoti@gmail.com>
2022-01-25 17:43:16 +0300

    [Reading a file asynchronously with Node JS] created a function ountStudents in the file 3-read_file_async.js -  It should accept a path in argument (same as in 2-read_file.js)

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -8,13 +8,15 @@ const countStudents = (path) => {
     const fields = {};
     for (const row of data) {
       const student = row.split(',');
-      if (!fields[student[3]])
+      if (!fields[student[3]]) {
         fields[student[3]] = [];
+      }
       fields[student[3]].push(student[0]);
     }
     for (const field in fields) {
-      if (field)
+      if (field) {
         console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
     }
   } catch (error) {
     throw new Error('Cannot load the database');
