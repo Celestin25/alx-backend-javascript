e2c79cc800fd159d1d1080ab4065a97b749d779c
coosoti <charlesosoti@gmail.com>
2022-01-27 14:33:56 +0300

    [Basic test with Mocha and Node assertion library] added unittest for a function that run tests using Mocha - npm test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function () {
   it('should be 1 + 3 = 4', function () {
@@ -11,7 +11,7 @@ describe('calculateNumber', function () {
   it('should be 1.2 + 3.7 = 5', function () {
     assert.equal(calculateNumber(1.2, 3.7), 5);
   });
-  it('should be 1.5 + 3.7 = 5', function () {
+  it('should be 1.5 + 3.7 = 6', function () {
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
